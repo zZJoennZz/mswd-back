8f869d17d4c417b1aa0722db8b5fa88a1179b008
Joenn <zzjoennzz@gmail.com>
2022-03-01 15:55:28 +0800

    added middleware for the signup

--- routes/api.php
+++ routes/api.php
@@ -31,8 +31,8 @@ use App\Http\Controllers\OtherController;
 // Route::post('validate', [AuthController::class, 'validateToken']);
 Route::post('signin', [AuthController::class, 'signin'])->name('login');
 
-Route::post('signup', [AuthController::class, 'signup']);
 Route::middleware('auth:api')->group(function () {
+    Route::post('signup', [AuthController::class, 'signup']);
     Route::post('validate', [AuthController::class, 'validateToken']);
     Route::get('getprofile', [AuthController::class, 'getProfile']);
     Route::put('updateprofile', [AuthController::class, 'update']);
